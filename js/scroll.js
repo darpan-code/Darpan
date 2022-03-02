6ef8ccfbc05cb637c9f79a650b427a79b6486395
Darpan Chakraborty <76438784+darpan-code@users.noreply.github.com>
2022-03-03 00:29:09 +0530

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+// navbar color change
+$(window).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 5);
+  });
+
+// nav item color 
+$(window).scroll(function () {
+    $('#home-nav').toggleClass('active', $(this).scrollTop() < 700);
+    $('#service-nav').toggleClass('active', $(this).scrollTop() > 700 && $(this).scrollTop() < 2000);
+    $('#resume-nav').toggleClass('active', $(this).scrollTop() > 2000);
+  });
