227ccf6bafa7f55d3f2e711b9b19e3732f28409c
Darpan Chakraborty <76438784+darpan-code@users.noreply.github.com>
2022-03-03 00:27:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- link favicon  -->
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    
+    <!-- link bootsrap css  -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- link font awesome -->
+    <link rel="stylesheet" href="css/all.min.css">
+
+    <!-- link google font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">
+    
+    <!-- link custome css -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>DARPAN | HOME</title>
+</head>
+<body>
+
+ <!-- home background image  -->
+ <div class="home-bg-image">
+        <img class="home-img" src="img/bg.png" alt="backgroung">
+    </div>
+
+    <!-- navigation bar -->
+    <div class="d-flex justify-content-center" >
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="div">
+        <div class="container">
+            <a class="navbar-brand" href="#"><i class="fa-solid fa-angle-left"></i><span style="font-weight: bold;">/</span>D<span style="font-weight: bold;">_</span>C<i class="fa-solid fa-angle-right"></i></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item mx-4">
+                <a class="nav-link active" aria-current="page" href="#" id="home-nav">HOME</a>
+                </li>
+                <li class="nav-item mx-4">
+                <a class="nav-link" href="#service" id="service-nav">SERVICE</a>
+                </li>
+                <li class="nav-item mx-4">
+                <a class="nav-link" href="#resume" id="resume-nav">RESUME</a>
+                </li>
+                <li class="nav-item mx-4">
+                <a class="nav-link" href="#">PROJECTS</a>
+                </li>
+                <li class="nav-item mx-4">
+                <a class="nav-link" href="#">CONTACT</a>
+                </li>
+                <li class="nav-item ms-4">
+                <a class="nav-link social-icon-linkdin" href="#"><i class="fa-brands fa-linkedin-in"></i> <span class="social-show">Linkdin</span></a>
+                </li>
+                <li class="nav-item ms-4">
+                <a class="nav-link social-icon-whatsapp" href="#"><i class="fa-brands fa-whatsapp"></i> <span class="social-show">Whatsapp</span></a>
+                </li>
+                <li class="nav-item ms-4">
+                <a class="nav-link social-icon-github" href="#"><i class="fa-brands fa-github"></i> <span class="social-show">Github</span></a>
+                </li>
+            
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </div>
+
+    <div class="d-flex align-items-end intro-main-height">
+    <div class="intro-sub">
+    <!-- tagline start -->
+    <div class="tag-line">
+        <p class="tag-question"><i class="fa-brands fa-hotjar"></i><span>Looking for a Developer!</span></p>
+        <p>I Am <span class="tag-type"></span></p>
+        <p class="intro-para">Software Engineer with good problem-solving skill and ability to perform well in a team. Passionate about technology.</p>
+        <div class="center">
+          <a href="#" class="get-btn">Contact</a>
+        </div>
+      </div>
+      </div>
+      <!-- tagline end -->
+
+    <!-- home image -->
+    <div class="pro-img">
+        <img src="img/img.png" alt="Professional Image">
+    </div>
+    </div>
+
+    <!-- home details  -->
+    <div class="home-details">
+        <div class="home-details-main">
+            <div class="home-details-left">
+                <p class="home-details-left-head">Personal Details</p>
+                <hr class="hr">
+                <p><span>Email:</span> darpanchakraborty.official@gmail.com</p>
+                <p><span>Languages:</span> English, Hindi, Bengali</p>
+                <p><span>Nationality:</span> Indian</p>
+            </div>
+            <div class="home-details-right">
+                <p class="home-details-right-head">I Am Darpan Chakraborty</p>
+                <p>Software Engineer with good problem-solving skill and ability to perform well in a team. Passionate about technology.</p>
+                <p class="home-details-right-sign">Darpan Chakraborty</p>
+            </div>
+        </div>
+    </div>
+    
+    
+    <!-- service section -->
+        <div class="service-title" id="service">
+            <div class="service-head">
+            Awesome Service
+            </div>
+            <div class="service-para">
+            Obviously I'm a Web Designer. Experienced with all stages of the development cycle for dynamic web projects.
+            </div>
+        </div>
+
+    <div class="service-container-main">
+        <!-- 1st container  -->
+        <div class="service-container">
+            <div class="service-container-img"><i class="fa-solid fa-laptop-code"></i></div>
+            <div class="service-container-head">Web Developmnet</div>
+            <div class="service-container-para">With lots of unique blocks, you can easily build a page without coding.</div>
+        </div>
+
+        <!-- 2d container  -->
+        <div class="service-container">
+            <div class="service-container-img"><i class="fa-solid fa-compass-drafting"></i></div>
+            <div class="service-container-head">Design & Planning</div>
+            <div class="service-container-para">With lots of unique blocks, you can easily build a page without coding.</div>
+        </div>
+
+        <!-- 3rd container  -->
+        <div class="service-container">
+            <div class="service-container-img"><i class="fa-solid fa-images"></i></div>
+            <div class="service-container-head">Adobe Photoshop</div>
+            <div class="service-container-para">With lots of unique blocks, you can easily build a page without coding.</div>
+        </div>
+
+        <!-- 4th container  -->
+        <div class="service-container">
+            <div class="service-container-img"><i class="fa-solid fa-square-poll-vertical"></i></div>
+            <div class="service-container-head">Digital marketing</div>
+            <div class="service-container-para">With lots of unique blocks, you can easily build a page without coding.</div>
+        </div>
+
+        <!-- 5th container  -->
+        <div class="service-container">
+            <div class="service-container-img"><i class="fa-solid fa-clock"></i></div>
+            <div class="service-container-head">24 / 7</div>
+            <div class="service-container-para">With lots of unique blocks, you can easily build a page without coding.</div>
+        </div>
+
+        <!-- 6th container  -->
+        <div class="service-container">
+            <div class="service-container-img"><i class="fa-solid fa-palette"></i></div>
+            <div class="service-container-head">Graphic Designer</div>
+            <div class="service-container-para">With lots of unique blocks, you can easily build a page without coding.</div>
+        </div>
+    </div>
+
+    <!-- Work Expertise section  -->
+
+    <div class="work-expertise-container">
+        <div class="work-expertise-img">
+            <img src="img/workimage.png" alt="work image">
+        </div>
+        <div class="work-expertise-main">
+            <div class="service-head">
+            Work Expertise
+            </div>
+            <!-- <div class="service-para">
+            Obviously I'm a Web Designer. Experienced with all stages of the development cycle for dynamic web projects.
+            </div> -->
+
+            <div class="work-experience-tech-container">
+                <!-- 1st tect  -->
+                <div class="work-experience-tech-img" style="color:#FF5733">
+                <i class="fa-brands fa-html5"></i>
+                </div>
+                <!-- 2nd tect  -->
+                <div class="work-experience-tech-img" style="color:#264de4">
+                <i class="fa-brands fa-css3"></i>
+                </div>
+                <!-- 3rd tect  -->
+                <div class="work-experience-tech-img" style="color:#f0db4f">
+                <i class="fa-brands fa-node-js"></i>
+                </div>
+                <!-- 4th tect  -->
+                <div class="work-experience-tech-img" style="color:#563d7c">
+                <i class="fa-brands fa-bootstrap"></i>
+                </div>
+                <!-- 5th tect  -->
+                <div class="work-experience-tech-img" style="color:#8993be">
+                <i class="fa-brands fa-php"></i>
+                </div>
+                <!-- 6th tect  -->
+                <div class="work-experience-tech-img" style="color:#0180CD">
+                <i class="fa-solid fa-c"></i>
+                </div>
+                <!-- 7th tect  -->
+                <div class="work-experience-tech-img" style="color:#171515">
+                <i class="fa-brands fa-github"></i>
+                </div>
+                <!-- 8th tect  -->
+                <div class="work-experience-tech-img" style="color:#00758F">
+                <i class="fa-solid fa-database"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!-- resume section  -->
+
+    <div class="service-title" id="resume">
+            <div class="service-head">
+            Resume
+            </div>
+            
+    </div>
+
+    <div class="resume-preview-container">
+        <div class="resume-preview-main">
+        <iframe src="https://drive.google.com/file/d/1F_oJJ9gBXDwtnehABEpTbgKEanTvjJvX/preview"></iframe>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <!-- link bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <!-- link font awesome  -->
+    <script src="js/all.min.js"></script>
+    
+    <!-- link custome js  -->
+    <script src="js/jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+    <script src="js/type.js"></script>
+    <script src="js/scroll.js"></script>
+</body>
+</html>
