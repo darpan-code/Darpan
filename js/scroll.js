992cdf3cf93e7b15d6379cca595052a27c557171
Darpan Chakraborty <76438784+darpan-code@users.noreply.github.com>
2022-03-03 17:28:39 +0530

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -8,5 +8,6 @@ $(window).scroll(function () {
     $('#home-nav').toggleClass('active', $(this).scrollTop() < 700);
     $('#service-nav').toggleClass('active', $(this).scrollTop() > 700 && $(this).scrollTop() < 2000);
     $('#resume-nav').toggleClass('active', $(this).scrollTop() > 2000 && $(this).scrollTop() < 4000);
-    $('#projects-nav').toggleClass('active', $(this).scrollTop() > 4000);
+    $('#projects-nav').toggleClass('active', $(this).scrollTop() > 4000 && $(this).scrollTop() < 4750);
+    $('#contact-nav').toggleClass('active', $(this).scrollTop() > 4750);
   });
